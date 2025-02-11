47951cd25c190ef56a7d6469db6e8fddde38f8f8
Elias Santana <166433239+Eliaas00@users.noreply.github.com>
2025-02-11 00:51:45 +0000

    aula foi legal

---
+++ unidade1/aula1/target/debug/aula1.d
@@ -0,0 +1,1 @@
+/workspaces/elias.santana.3nb/unidade1/aula1/target/debug/aula1: /workspaces/elias.santana.3nb/unidade1/aula1/src/main.rs
---
+++ unidade1/aula1/target/debug/deps/aula1-0feb40cd98cdc893.d
@@ -0,0 +1,5 @@
+/workspaces/elias.santana.3nb/unidade1/aula1/target/debug/deps/aula1-0feb40cd98cdc893: src/main.rs
+
+/workspaces/elias.santana.3nb/unidade1/aula1/target/debug/deps/aula1-0feb40cd98cdc893.d: src/main.rs
+
+src/main.rs:
