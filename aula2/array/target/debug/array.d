0f38be37a2433faf87487206d3f078c618843feb
Elias Santana <166433239+Eliaas00@users.noreply.github.com>
2025-02-24 23:54:09 +0000

    feat: remove

--- aula2/array/target/debug/array.d
+++
@@ -1,1 +0,0 @@
-/workspaces/elias.santana.3nb/aula2/array/target/debug/array: /workspaces/elias.santana.3nb/aula2/array/src/main.rs
--- aula2/array/target/debug/deps/array-6e278f5fa8612e71.d
+++
@@ -1,5 +0,0 @@
-/workspaces/elias.santana.3nb/aula2/array/target/debug/deps/array-6e278f5fa8612e71: src/main.rs
-
-/workspaces/elias.santana.3nb/aula2/array/target/debug/deps/array-6e278f5fa8612e71.d: src/main.rs
-
-src/main.rs:
