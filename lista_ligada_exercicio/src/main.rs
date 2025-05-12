c3a39a396e500f22e31c3e5b55fb9d34fb151453
Elias Santana <166433239+Eliaas00@users.noreply.github.com>
2025-05-12 23:49:36 +0000

    feat: lista 12/05

---
+++ lista_ligada_exercicio/src/main.rs
@@ -0,0 +1,12 @@
+use std::collections::LinkedList;
+
+fn main() {
+    // Crie uma lista ligada vazia
+    let mut frases = LinkedList::new();
+    
+    // TODO: Adicione 10 frases motivacionais à lista
+    
+    // TODO: Acesse e imprima o terceiro elemento
+    
+    // TODO: Imprima o tamanho total da lista
+}
